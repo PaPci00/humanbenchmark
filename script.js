460fe9a22452c58365dee5a1938f4354a25417a8
PaPci00 <99023795+PaPci00@users.noreply.github.com>
2023-02-20 20:52:51 +0100

    beta testy v1

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ let liczba = 0;
 function teleport(){
     let x = Math.floor(Math.random()*width.value +1);
     let y = Math.floor(Math.random()*height.value +1);
-    click.style = "margin-top: "+y+"px; margin-left: "+x+"px;";
+    click.style.marginTop = `${y}px`;
+    click.style.marginLeft = `${x}px`;
     liczba = liczba + 1;
-    click.innerHTML = liczba;
+    wynik.innerHTML = liczba;
 }
 
+let odleglosc = 0;
+
 function rozmiar(){
+    odleglosc = kulka.value;
     let smallwidth = parseInt(width.value)+50;
     let smallheight = parseInt(height.value)+50;
     if(smallwidth > 1050 || smallheight > 850)
@@ -45,4 +49,12 @@ function faza(){
     ilebb = blue.value;
 
     box.style.backgroundColor = `rgb(${ilerr}, ${ilegg}, ${ilebb})`;
+}
+
+let kulkoni = 0;
+
+function kuleczka(){
+    kulkoni = kulka.value;
+    click.style.height = `${kulkoni}px`;
+    click.style.width = `${kulkoni}px`;
 }
